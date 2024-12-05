4c7b849a3a14b26d1d7723c620460bb5aafd4553
Darius Cleaver <dcleaver@usloaner-mba02.lan>
2024-12-04 22:24:48 -0600

    array hw

---
+++ javascript-arrays/script.js
@@ -0,0 +1,29 @@
+//let tasks = ["wrap presents", "do dishes", "clean my bedroom"];
+//alert(tasks[0]);
+//tasks[0] = "make dinner";
+//alert(tasks);
+//tasks.push("take a shower");
+//alert(tasks);
+//tasks.pop();
+//alert(tasks);
+//tasks.splice(1, 1);
+//alert(tasks);
+
+let groceries = ["milk", "fruit", "bread"];
+alert(groceries);
+groceries.push("eggs");
+groceries.push("cereal");
+alert(groceries);
+groceries.pop();
+alert(groceries);
+groceries.splice(1, 0, "ice cream");
+alert(groceries);
+groceries.splice(2, 1);
+alert(groceries);
+
+let nums = [1, 2, 3];
+alert(nums);
+nums.splice(0, 0, 4);
+alert(nums);
+nums.splice(1, 1, 5);
+alert(nums);
