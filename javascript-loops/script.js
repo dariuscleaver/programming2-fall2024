2382f98d1c9ba20b1a07f2257edc282be630a2a8
Darius Cleaver <dcleaver@usloaner-mba02.fwparker.org>
2024-12-06 10:02:39 -0600

    arrays

---
+++ javascript-loops/script.js
@@ -0,0 +1,14 @@
+//let fastfood = ["hamburger", "fries", "milkshake", "nuggets"];
+//for (let i = 0; i < fastfood.length; i++) {
+//    console.log(fastfood[i]);
+//}
+//let numbers = [45, 23, 78];
+//let doubled = [];
+//for (let i = 0; i < numbers.length; i++) {
+//    doubled.push(2*numbers[i]);
+//}
+//console.log(doubled);
+
+for (let i = 0; i < 100; i++) {
+    console.log("Darius");
+}
